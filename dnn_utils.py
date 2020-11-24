fa92c8e03994a3eef207f5ef4d5dc1b5f6c08fd7
hugoyervides <victor.covarrubias@hotmail.com>
2020-11-23 21:50:55 -0600

    bug fixes

--- dnn_utils.py
+++ dnn_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import h5py
+import matplotlib.pyplot as plt
 
 def load_data(dataset_path):
     train_dataset = h5py.File(dataset_path, "r")
@@ -10,12 +11,17 @@ def load_data(dataset_path):
     test_set_x_orig = np.array(test_dataset["test_img"][:]) # your test set features
     test_set_y_orig = np.array(test_dataset["test_labels"][:]) # your test set labels
 
+    dev_dataset = h5py.File(dataset_path, "r")
+    dev_set_x_orig = np.array(dev_dataset["dev_img"][:]) # your test set features
+    dev_set_y_orig = np.array(dev_dataset["dev_labels"][:]) # your test set labels
+
     classes = np.array(test_dataset["list_classes"][:]) # the list of classes
     
     train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
     test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
-    
-    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+    dev_set_y_orig = dev_set_y_orig.reshape((1, dev_set_y_orig.shape[0]))
+
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, dev_set_x_orig, dev_set_y_orig, classes
 
 def sigmoid(Z):
     """
@@ -378,3 +384,40 @@ def predict(X, y, parameters):
     print("Accuracy: "  + str(np.sum((p == y)/m)))
         
     return p
+
+def accuracy(X, y, parameters):
+    m = X.shape[1]
+    n = len(parameters) // 2 # number of layers in the neural network
+    p = np.zeros((1,m))
+    
+    # Forward propagation
+    probas, caches = L_model_forward(X, parameters)
+
+    
+    # convert probas to 0/1 predictions
+    for i in range(0, probas.shape[1]):
+        if probas[0,i] > 0.5:
+            p[0,i] = 1
+        else:
+            p[0,i] = 0
+
+    return np.sum((p == y)/m)
+
+def print_mislabeled_images(classes, X, y, p):
+    """
+    Plots images where predictions and truth were different.
+    X -- dataset
+    y -- true labels
+    p -- predictions
+    """
+    a = p + y
+    mislabeled_indices = np.asarray(np.where(a == 1))
+    plt.rcParams['figure.figsize'] = (40.0, 40.0) # set default size of plots
+    num_images = len(mislabeled_indices[0])
+    for i in range(num_images):
+        index = mislabeled_indices[1][i]
+        
+        plt.subplot(2, num_images, i + 1)
+        plt.imshow(X[:,index].reshape(128,128,3), interpolation='nearest')
+        plt.axis('off')
+        plt.title("Prediction: " + classes[int(p[0,index])].decode("utf-8") + " \n Class: " + classes[y[0,index]].decode("utf-8"))
